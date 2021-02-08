de71f899be63e78dbe43c7cd89e651ad93378876
Jacob Stephens <jacobstephens2@gmail.com>
2021-02-08 12:14:59 -0800

    initial commit, have server running, get userdata working and get by indiv id working

--- api/server.js
+++ api/server.js
@@ -1,3 +1,37 @@
-// BUILD YOUR SERVER HERE
+const express = require("express")
+const generate = require("shortid").generate
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+
+const server = express();
+server.use(express.json())
+
+
+let allUsers = [
+  { id:generate(), name: 'Ed Carter', bio: 'hero' },
+  { id:generate(), name: 'Mary Edwards', bio: 'super hero' },
+]
+
+
+server.get("/api/users", (req,res)=>{
+  res.status(200).json(allUsers)
+})
+
+
+server.get("/api/users/:id", (req,res)=>{
+  const indivId = req.params.id
+  const indivEmp = allUsers.find(user => user.id === indivId)
+
+  if(!indivEmp){
+    res.status(404).json({message: `id: ${indivId} does not exist`})
+  }else{
+    res.status(200).json(indivEmp)
+  }
+})
+
+
+server.use("*",(req,res)=>{
+    res.status(404).json({message:"404 Not found"})
+})
+
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
